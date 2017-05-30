6c102995d9df9589ee742ae71e9bd3621d7af16d
Jon Ramer <jramer87@gmail.com>
2017-05-30 14:41:31 -0500

    adds image to card header

--- react-tutorials/albums/src/components/AlbumDetail.js
+++ react-tutorials/albums/src/components/AlbumDetail.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+
   return (
     <Card>
       <CardSection>
-        <View></View>
+        <View>
+          <Image
+            style={styles.thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
         <View style={styles.headerContentStyle}>
-          <Text>{props.album.title}</Text>
-          <Text>{props.album.artist}</Text>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
         </View>
       </CardSection>
     </Card>
@@ -21,6 +28,10 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
   }
 }
 
