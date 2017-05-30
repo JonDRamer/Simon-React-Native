99e2cb90d86ed2e3b5dc931f4b2e792eecf566f1
Jon Ramer <jramer87@gmail.com>
2017-05-30 13:44:41 -0500

    adds card component boilerplate, completes api call

--- react-tutorials/albums/index.ios.js
+++ react-tutorials/albums/index.ios.js
@@ -3,13 +3,17 @@
 
 // Import a library to help create a compoenent
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a Component
 const App = () =>
    (
-    <Header /> 
+     <View>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
   );
 
 // Render it to the device
