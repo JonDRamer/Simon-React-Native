9eb73b9f460e307b5f3d21227989750a8e8377b9
Jon Ramer <jramer87@gmail.com>
2017-05-30 14:51:57 -0500

    adds styles for album art

--- react-tutorials/albums/src/components/AlbumDetail.js
+++ react-tutorials/albums/src/components/AlbumDetail.js
@@ -4,21 +4,24 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={styles.thumbnailContainerStyle}>
           <Image
             style={styles.thumbnailStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={styles.headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={styles.headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={styles.imageStyle} source={{ uri: image }} />
       </CardSection>
     </Card>
   );
@@ -29,9 +32,23 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 }
 
