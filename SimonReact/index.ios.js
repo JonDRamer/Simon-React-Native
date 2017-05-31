c68f1543ebcb75e8c80b93738c3fa3db141461bd
Jon Ramer <jramer87@gmail.com>
2017-05-31 15:45:28 -0500

    adds console, header, buttons, and consoleText

--- SimonReact/index.ios.js
+++ SimonReact/index.ios.js
@@ -1,7 +1,7 @@
 'use-strict';
 
 import React from 'react';
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry, View, Text } from 'react-native';
 //Create a Component
 const App = () =>
    (
@@ -10,6 +10,21 @@ const App = () =>
           <View style={styles.yellowPad}>
           </View>
           <View style={styles.bluePad}>
+          </View>
+          <View style={styles.consoleBackground}>
+          </View>
+          <View style={styles.console}>
+            <Text style={styles.consoleHeaderText}>simon</Text>
+            <View style={styles.buttonContainer}>
+              <View style={styles.button}></View>
+              <View style={styles.button2}></View>
+              <View style={styles.button}></View>
+            </View>
+            <View style={styles.settings}>
+              <Text style={styles.consoleText}>Last</Text>
+              <Text style={styles.consoleText}>Start</Text>
+              <Text style={styles.consoleText}>Long</Text>
+            </View>
           </View>
           <View style={styles.redPad}>
           </View>
@@ -40,36 +55,147 @@ const App = () =>
       shadowOpacity: .4
     },
     yellowPad: {
-      height: 160,
-      width: 160,
-      marginTop: 10,
-      marginLeft: 10,
+      height: 150,
+      width: 150,
+      marginTop: 17,
+      marginLeft: 17,
       backgroundColor: "#f4f401",
-      borderTopLeftRadius: 200
+      borderTopLeftRadius: 200,
+      shadowOffset: {
+        width: 1,
+        height: 1
+      },
+      shadowColor: '#bfbfbf',
+      shadowOpacity: 1
     },
     bluePad: {
+      height: 150,
+      width: 150,
+      marginTop: 17,
+      marginLeft: 15,
+      backgroundColor: "#0099ff",
+      borderTopRightRadius: 200,
+      shadowOffset: {
+        width: 1,
+        height: 1
+      },
+      shadowColor: '#bfbfbf',
+      shadowOpacity: 1
+    },
+    consoleBackground: {
+      position: 'absolute',
+      marginLeft: 95,
+      marginTop: 97,
+      zIndex: 1,
       height: 160,
       width: 160,
-      marginTop: 10,
-      marginLeft: 10,
-      backgroundColor: "#0099ff",
-      borderTopRightRadius: 200
+      borderRadius: 100,
+      backgroundColor: '#262626',
+      shadowOffset: {
+        width: 2,
+        height: 2
+      },
+      shadowColor: 'black',
+      shadowOpacity: .2
+    },
+    console: {
+      position: 'absolute',
+      justifyContent: 'flex-start',
+      marginLeft: 105,
+      marginTop: 107,
+      zIndex: 2,
+      height: 140,
+      width: 140,
+      backgroundColor: 'silver',
+      borderRadius: 100,
+      shadowOffset: {
+        width: 5,
+        height: 5
+      },
+      shadowColor: '#bfbfbf',
+      shadowOpacity: .2
+    },
+    consoleHeaderText: {
+      alignSelf: 'center',
+      marginTop: 15,
+      fontSize: 30,
+      fontWeight: '600',
+    },
+    buttonContainer: {
+      position: 'absolute',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      alignSelf: 'center',
+      marginTop: 50,
+      width: 130,
+      height: 30
+    },
+    button: {
+      height: 10,
+      width: 10,
+      borderRadius: 100,
+      backgroundColor: 'yellow',
+      alignSelf: 'center',
+      shadowOffset: {
+        width: 1,
+        height: 1
+      },
+      shadowColor: '#262626',
+      shadowOpacity: 1
+    },
+    button2: {
+      height: 10,
+      width: 10,
+      borderRadius: 100,
+      backgroundColor: "#e60000",
+      alignSelf: 'center',
+      shadowOffset: {
+        width: 1,
+        height: 1
+      },
+      shadowColor: '#262626',
+      shadowOpacity: 1
+    },
+    settings: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      alignSelf: 'center',
+      marginTop: 25,
+      width: 130,
+      height: 17
+    },
+    consoleText: {
+      fontSize: 10
     },
     redPad: {
-      height: 160,
-      width: 160,
-      marginTop: 10,
-      marginLeft: 10,
+      height: 150,
+      width: 150,
+      marginTop: 15,
+      marginLeft: 17,
       backgroundColor: "#e60000",
-      borderBottomLeftRadius: 200
+      borderBottomLeftRadius: 200,
+      shadowOffset: {
+        width: 1,
+        height: 1
+      },
+      shadowColor: '#bfbfbf',
+      shadowOpacity: 1
     },
     greenPad: {
-      height: 160,
-      width: 160,
-      marginTop: 10,
-      marginLeft: 10,
+      height: 150,
+      width: 150,
+      marginTop: 15,
+      marginLeft: 15,
       backgroundColor: "#009900",
-      borderBottomRightRadius: 200
+      borderBottomRightRadius: 200,
+      shadowOffset: {
+        width: 1,
+        height: 1
+      },
+      shadowColor: '#bfbfbf',
+      shadowOpacity: 1
     }
   }
 
